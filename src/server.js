9d2234e177497d7f3fb3a2d8c1184ea4f87c2ec1
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2023-01-17 23:53:40 +0300

    HTTP Methods added

--- src/server.js
+++ src/server.js
@@ -10,6 +10,18 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
   })
 
+app.post('/', (req, res) => {
+    res.send('Here we go nodee uppp! Created msg');
+  })
+
+app.put('/', (req, res) => {
+    res.send('Here we go nodee uppp! Updated msg');
+  })  
+
+app.delete('/', (req, res) => {
+    res.send('Here we go nodee uppp! dlt msg');
+  })
+
 app.listen(process.env.PORT, () =>
   console.log(`app is ready and listening on port ${process.env.PORT}!`),
 )
