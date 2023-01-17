8a8b531ce8e6e6356cc381e06ee742ce6ce4a9d7
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2023-01-17 23:59:58 +0300

    HTTP Methods added with sample data

--- src/server.js
+++ src/server.js
@@ -6,21 +6,83 @@ const app = express()
 
 app.use(cors())
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use((req, res, next) => {
+  req.me = users[1]
+  next();
+})
+
+app.get('/users', (req, res) => {
+  return res.send(Object.values(users))
   })
 
-app.post('/', (req, res) => {
-    res.send('Here we go nodee uppp! Created msg');
+app.get('/users/:userId', (req, res) => {
+  return res.send(users[req.params.userId])
   })
 
-app.put('/', (req, res) => {
-    res.send('Here we go nodee uppp! Updated msg');
+app.post('/users', (req, res) => {
+  return res.send('POST HTTP method on user resource')
+  })
+
+app.put('/users/:userId', (req, res) => {
+  return res.send(
+    `PUT HTTP method on user/${req.params.userId} resource`,
+  )
   })  
 
-app.delete('/', (req, res) => {
-    res.send('Here we go nodee uppp! dlt msg');
+app.delete('/users/:userId', (req, res) => {
+  return res.send(
+    `DLT HTTP method on user/${req.params.userId} resource`,
+  )
   })
+
+  app.get('/messages', (req, res) => {
+    return res.send(Object.values(messages))
+    })
+  
+  app.get('/messages/:messageId', (req, res) => {
+    return res.send(messages[req.params.messageId])
+    })
+
+  app.post('/messages', (req, res) => {
+  const id = uuidv4();
+  const message = {
+    id,
+    text: req.body.text,
+    userId: req.me.id
+  };
+
+  messages[id] = message;
+
+  return res.send(message);
+})
+  
+    
+
+  let users = {
+    1: {
+      id: '1',
+      username: 'Conrad P.B',
+    },
+    2: {
+      id: '2',
+      username: 'Tia reed',
+    },
+  };
+  
+  let messages = {
+    1: {
+      id: '1',
+      text: 'Hello all, How u today?',
+      userId: '1',
+    },
+    2: {
+      id: '2',
+      text: 'Itsss all gooodd brahh!!',
+      userId: '2',
+    },
+  };
 
 app.listen(process.env.PORT, () =>
   console.log(`app is ready and listening on port ${process.env.PORT}!`),
