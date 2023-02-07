6783b487df86c3121c51e64a37227ac18c3f2ea0
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2023-02-07 23:59:29 +0300

    authentication middleware added and welcome route created

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import User from '../controllers/user'
+import verifyToken from '../middleware/authMiddleware'
 
 const router = Router()
 
@@ -12,6 +13,8 @@ router.get('/:userId', user.fetchUser)
 router.post('/', user.registerUser)
 
 router.post('/login', user.loginUser)
+
+router.post('/welcome', verifyToken, user.welcomeUser)
 
 
 export default router
