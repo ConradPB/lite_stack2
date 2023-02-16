280185a3957365ec782f65609ea514c0f71a9d59
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2023-02-16 15:45:26 +0300

    configuration of tests

--- src/models/index.js
+++ src/models/index.js
@@ -8,8 +8,7 @@ import testEnv from '../../config/test.env'
 
 const connectDb = async () => {
   const mongoUri = env.NODE_ENV === 'test' ? testEnv.MONGO_URI2 : env.MONGO_URI;
-  mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false,
-    useCreateIndex: true, });
+  mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true, });
 }
 mongoose.set('strictQuery', true) //enables strict query mode in mongoDB
 
--- test/question.test.js
+++ test/question.test.js
@@ -1,13 +1,23 @@
 import request from 'supertest'
 import app from '../src/app'
+import mongoose from 'mongoose'
+
 jest.setTimeout(10000);
 
+beforeEach((done) => {
+  mongoose.connect("mongodb://localhost:27017/JestDB",
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => done());
+});
 
+afterEach(async () => {
+  await mongoose.connection.dropDatabase();
+  await mongoose.connection.close();
+});
 
 describe('Test the root path', () => {
     test('It should respond to the GET method', async () => {
       const response = await request(app).get('/')
       expect(response.statusCode).toBe(200)
     })
-  })
-  
+})
